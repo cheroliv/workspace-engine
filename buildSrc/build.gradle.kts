8ffaef5c85182f974575f8a36f18bb05cbd95a01
cheroliv <cheroliv@laposte.net>
2025-05-24 00:17:14 +0200

    Standardize constant references with explicit Constants object.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,9 +1,3 @@
-import Build_gradle.Constants.arrowKtVersion
-import Build_gradle.Constants.asciidoctorGradleVersion
-import Build_gradle.Constants.jacksonVersion
-import Build_gradle.Constants.jgitVersion
-import Build_gradle.Constants.langchain4jVersion
-
 plugins { `kotlin-dsl` }
 
 repositories {
@@ -22,6 +16,7 @@ repositories {
 object Constants {
     const val langchain4jVersion = "1.0.0-beta1"//"0.36.2"
     const val asciidoctorGradleVersion = "4.0.0-alpha.1"
+//    const val asciidoctorGradleVersion = "4.0.4"
     const val jacksonVersion = "2.17.2"//2.18.0
     const val arrowKtVersion = "1.2.4"
     const val jgitVersion = "6.10.0.202406032230-r"
@@ -33,23 +28,23 @@ dependencies {
         "com.avast.gradle:gradle-docker-compose-plugin:0.17.6",
         "com.github.node-gradle:gradle-node-plugin:7.0.1",
         "jakarta.xml.bind:jakarta.xml.bind-api:4.0.2",
-        "com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion",
-        "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion",
-        "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion",
-        "com.fasterxml.jackson.module:jackson-module-jsonSchema:$jacksonVersion",
-        "org.eclipse.jgit:org.eclipse.jgit:$jgitVersion",
-        "org.eclipse.jgit:org.eclipse.jgit.archive:$jgitVersion",
-        "org.eclipse.jgit:org.eclipse.jgit.ssh.jsch:$jgitVersion",
-        "io.arrow-kt:arrow-core:$arrowKtVersion",
-        "io.arrow-kt:arrow-fx-coroutines:$arrowKtVersion",
+        "com.fasterxml.jackson.module:jackson-module-kotlin:${Constants.jacksonVersion}",
+        "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:${Constants.jacksonVersion}",
+        "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Constants.jacksonVersion}",
+        "com.fasterxml.jackson.module:jackson-module-jsonSchema:${Constants.jacksonVersion}",
+        "org.eclipse.jgit:org.eclipse.jgit:${Constants.jgitVersion}",
+        "org.eclipse.jgit:org.eclipse.jgit.archive:${Constants.jgitVersion}",
+        "org.eclipse.jgit:org.eclipse.jgit.ssh.jsch:${Constants.jgitVersion}",
+        "io.arrow-kt:arrow-core:${Constants.arrowKtVersion}",
+        "io.arrow-kt:arrow-fx-coroutines:${Constants.arrowKtVersion}",
         "io.arrow-kt:arrow-integrations-jackson-module:0.14.1",
         "org.apache.poi:poi-ooxml:5.2.5",
         "org.jbake:jbake-gradle-plugin:5.5.0",
         "org.slf4j:slf4j-simple:2.0.16",
         "org.asciidoctor:asciidoctorj-diagram:2.3.1",
-        "org.asciidoctor:asciidoctor-gradle-jvm-slides:$asciidoctorGradleVersion",
-        "org.asciidoctor:asciidoctor-gradle-base:$asciidoctorGradleVersion",
-        "org.asciidoctor:asciidoctor-gradle-jvm-gems:$asciidoctorGradleVersion",
+        "org.asciidoctor:asciidoctor-gradle-jvm-slides:${Constants.asciidoctorGradleVersion}",
+        "org.asciidoctor:asciidoctor-gradle-base:${Constants.asciidoctorGradleVersion}",
+        "org.asciidoctor:asciidoctor-gradle-jvm-gems:${Constants.asciidoctorGradleVersion}",
         "com.burgstaller:okhttp-digest:1.10",
         "org.ysb33r.gradle:grolifant:0.12.1",
         "com.avast.gradle:gradle-docker-compose-plugin:0.14.2",
@@ -61,12 +56,12 @@ dependencies {
 //        "org.jetbrains.kotlin:kotlin-stdlib",
 //        "commons-io:commons-io:$commonsIoVersion",
 //        "org.tukaani:xz:1.9",
-        "dev.langchain4j:langchain4j:$langchain4jVersion",
-        "dev.langchain4j:langchain4j-ollama:$langchain4jVersion",
-        "dev.langchain4j:langchain4j-hugging-face:$langchain4jVersion",
-        "dev.langchain4j:langchain4j-google-ai-gemini:$langchain4jVersion",
-        "dev.langchain4j:langchain4j-mistral-ai:$langchain4jVersion",
-        "dev.langchain4j:langchain4j-pgvector:$langchain4jVersion",
+        "dev.langchain4j:langchain4j:${Constants.langchain4jVersion}",
+        "dev.langchain4j:langchain4j-ollama:${Constants.langchain4jVersion}",
+        "dev.langchain4j:langchain4j-hugging-face:${Constants.langchain4jVersion}",
+        "dev.langchain4j:langchain4j-google-ai-gemini:${Constants.langchain4jVersion}",
+        "dev.langchain4j:langchain4j-mistral-ai:${Constants.langchain4jVersion}",
+        "dev.langchain4j:langchain4j-pgvector:${Constants.langchain4jVersion}",
 
 //        "org.testcontainers:testcontainers:$testcontainersVersion",
 //        "org.testcontainers:ollama:$testcontainersVersion",
