2682a52fa228b969b4b7076249fcda05c25b221b
cheroliv <cheroliv@laposte.net>
2025-06-12 00:53:33 +0200

    Updates Kotlin version to 2.1.21 and cleans up unused dependencies in gradle.properties; refines plugin management in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,4 @@
 pluginManagement {
-    @Suppress("unused")
-    val asciidoctorGradleVersion = "4.0.0-alpha.1"
-
     repositories {
         google()
         mavenCentral()
@@ -12,26 +9,14 @@ pluginManagement {
         maven("https://archiva-repository.apache.org/archiva/repository/public/")
     }
     plugins {
-//        kotlin("multiplatform").version(extra["kotlin.version"].toString())
         id("org.jbake.site").version(extra["jbake-gradle.version"].toString())
         id("com.github.node-gradle.node").version(extra["node-gradle.version"].toString())
-//        id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
-//        id("org.asciidoctor:asciidoctor-gradle-jvm-slides") version (asciidoctorGradleVersion)
-//        id("org.asciidoctor:asciidoctor-gradle-base") version (asciidoctorGradleVersion)
-//        id("org.asciidoctor:asciidoctor-gradle-jvm-gems") version (asciidoctorGradleVersion)
         kotlin("jvm").version(extra["kotlin.version"].toString())
         kotlin("plugin.serialization").version(extra["kotlin.version"].toString())
         kotlin("plugin.allopen").version(extra["kotlin.version"].toString())
         kotlin("plugin.noarg").version(extra["kotlin.version"].toString())
-        kotlin("plugin.spring").version(extra["kotlin.version"].toString())
-        id("org.springframework.boot").version(extra["springboot.version"].toString())
-        id("io.spring.dependency-management").version(extra["spring_dependency_management.version"].toString())
     }
 }
-
-//plugins {
-//    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-//}
 
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
