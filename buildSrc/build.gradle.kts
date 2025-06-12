443135d49c5c9fbe96e8fed2804c059d80a20d9c
cheroliv <cheroliv@laposte.net>
2025-06-12 19:44:56 +0200

    Refactors Gradle build script to use version keys for dependency management and adds gradle.properties for version configuration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,17 +13,23 @@ repositories {
     ).forEach(::maven)
 }
 
-object Constants {
-//    const val langchain4jVersion = "1.0.1"
-//    const val langchain4ExtensionVersion = "1.0.1-beta6"
-    const val langchain4jVersion = "1.0.0-beta1"
-    const val langchain4ExtensionVersion = "1.0.0-beta1"
-    const val asciidoctorGradleVersion = "4.0.0-alpha.1"
-//        const val asciidoctorGradleVersion = "4.0.4"
-    const val jacksonVersion = "2.17.2"
-    const val arrowKtVersion = "1.2.4"
-    const val jgitVersion = "6.10.0.202406032230-r"
-}
+
+val asciidoctorGradleVersionKey = "asciidoctor-gradle.version"
+val jacksonVersionKey = "jackson.version"
+val arrowKtVersionKey = "arrow-kt.version"
+val jgitVersionKey = "jgit.version"
+val langchain4jVersionKey = "langchain4j.version"
+val langchain4jExtensionVersionKey = "langchain4j-extension.version"
+
+val Project.versions: Map<String, String>
+    get() = mapOf(
+        asciidoctorGradleVersionKey to project.properties[asciidoctorGradleVersionKey].toString(),
+        jacksonVersionKey to project.properties[jacksonVersionKey].toString(),
+        arrowKtVersionKey to project.properties[arrowKtVersionKey].toString(),
+        jgitVersionKey to project.properties[jgitVersionKey].toString(),
+        langchain4jVersionKey to project.properties[langchain4jVersionKey].toString(),
+        langchain4jExtensionVersionKey to project.properties[langchain4jExtensionVersionKey].toString()
+    )
 
 dependencies {
     setOf(
@@ -31,23 +37,23 @@ dependencies {
         "com.avast.gradle:gradle-docker-compose-plugin:0.17.6",
         "com.github.node-gradle:gradle-node-plugin:7.0.1",
         "jakarta.xml.bind:jakarta.xml.bind-api:4.0.2",
-        "com.fasterxml.jackson.module:jackson-module-kotlin:${Constants.jacksonVersion}",
-        "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:${Constants.jacksonVersion}",
-        "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Constants.jacksonVersion}",
-        "com.fasterxml.jackson.module:jackson-module-jsonSchema:${Constants.jacksonVersion}",
-        "org.eclipse.jgit:org.eclipse.jgit:${Constants.jgitVersion}",
-        "org.eclipse.jgit:org.eclipse.jgit.archive:${Constants.jgitVersion}",
-        "org.eclipse.jgit:org.eclipse.jgit.ssh.jsch:${Constants.jgitVersion}",
-        "io.arrow-kt:arrow-core:${Constants.arrowKtVersion}",
-        "io.arrow-kt:arrow-fx-coroutines:${Constants.arrowKtVersion}",
+        "com.fasterxml.jackson.module:jackson-module-kotlin:${versions[jacksonVersionKey]}",
+        "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:${versions[jacksonVersionKey]}",
+        "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${versions[jacksonVersionKey]}",
+        "com.fasterxml.jackson.module:jackson-module-jsonSchema:${versions[jacksonVersionKey]}",
+        "org.eclipse.jgit:org.eclipse.jgit:${versions[jgitVersionKey]}",
+        "org.eclipse.jgit:org.eclipse.jgit.archive:${versions[jgitVersionKey]}",
+        "org.eclipse.jgit:org.eclipse.jgit.ssh.jsch:${versions[jgitVersionKey]}",
+        "io.arrow-kt:arrow-core:${versions[arrowKtVersionKey]}",
+        "io.arrow-kt:arrow-fx-coroutines:${versions[arrowKtVersionKey]}",
         "io.arrow-kt:arrow-integrations-jackson-module:0.14.1",
         "org.apache.poi:poi-ooxml:5.2.5",
         "org.jbake:jbake-gradle-plugin:5.5.0",
         "org.slf4j:slf4j-simple:2.0.16",
         "org.asciidoctor:asciidoctorj-diagram:2.3.1",
-        "org.asciidoctor:asciidoctor-gradle-jvm-slides:${Constants.asciidoctorGradleVersion}",
-        "org.asciidoctor:asciidoctor-gradle-base:${Constants.asciidoctorGradleVersion}",
-        "org.asciidoctor:asciidoctor-gradle-jvm-gems:${Constants.asciidoctorGradleVersion}",
+        "org.asciidoctor:asciidoctor-gradle-jvm-slides:${versions[asciidoctorGradleVersionKey]}",
+        "org.asciidoctor:asciidoctor-gradle-base:${versions[asciidoctorGradleVersionKey]}",
+        "org.asciidoctor:asciidoctor-gradle-jvm-gems:${versions[asciidoctorGradleVersionKey]}",
         "com.burgstaller:okhttp-digest:1.10",
         "org.ysb33r.gradle:grolifant:0.12.1",
         "com.avast.gradle:gradle-docker-compose-plugin:0.14.2",
@@ -59,12 +65,12 @@ dependencies {
 //        "org.jetbrains.kotlin:kotlin-stdlib",
 //        "commons-io:commons-io:$commonsIoVersion",
 //        "org.tukaani:xz:1.9",
-        "dev.langchain4j:langchain4j:${Constants.langchain4jVersion}",
-        "dev.langchain4j:langchain4j-ollama:${Constants.langchain4ExtensionVersion}",
-        "dev.langchain4j:langchain4j-hugging-face:${Constants.langchain4ExtensionVersion}",
-        "dev.langchain4j:langchain4j-google-ai-gemini:${Constants.langchain4ExtensionVersion}",
-        "dev.langchain4j:langchain4j-mistral-ai:${Constants.langchain4ExtensionVersion}",
-        "dev.langchain4j:langchain4j-pgvector:${Constants.langchain4ExtensionVersion}",
+        "dev.langchain4j:langchain4j:${versions[langchain4jVersionKey]}",
+        "dev.langchain4j:langchain4j-ollama:${versions[langchain4jExtensionVersionKey]}",
+        "dev.langchain4j:langchain4j-hugging-face:${versions[langchain4jExtensionVersionKey]}",
+        "dev.langchain4j:langchain4j-google-ai-gemini:${versions[langchain4jExtensionVersionKey]}",
+        "dev.langchain4j:langchain4j-mistral-ai:${versions[langchain4jExtensionVersionKey]}",
+        "dev.langchain4j:langchain4j-pgvector:${versions[langchain4jExtensionVersionKey]}",
 //        "org.testcontainers:testcontainers:$testcontainersVersion",
 //        "org.testcontainers:ollama:$testcontainersVersion",
     ).forEach(::implementation)
