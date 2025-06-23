e5d6030414c4d57c75762bde200fa0130a17e1eb
cheroliv <cheroliv@laposte.net>
2025-06-23 03:37:48 +0200

    refactor: Consolidate toolchains plugin declaration in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,10 +7,8 @@ pluginManagement {
         kotlin("jvm").version("2.1.21")
         id("org.jbake.site").version(extra["jbake-gradle.version"].toString())
         id("com.github.node-gradle.node").version(extra["node-gradle.version"].toString())
+        id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
     }
-}
-plugins {
-    this.id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 @Suppress("UnstableApiUsage")
