6c4ccfe2753c8aacf154a69dce6d5d34e11f711a
cheroliv <cheroliv@laposte.net>
2025-06-26 20:54:02 +0200

    Refactors dev.nix to replace Maven with Gradle and updates workspace commands for Gradle

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,52 +3,26 @@
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
-
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.zulu17
+    pkgs.gradle # CHANGED: Remplacé pkgs.maven par pkgs.gradle
   ];
-
   # Sets environment variables in the workspace
   env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "vscjava.vscode-java-pack"
     ];
-
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
-    # Workspace lifecycle hooks
     workspace = {
-      # Runs when a workspace is first created
+      # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        install = "gradle clean build"; # CHANGED: Commande Gradle équivalente à "mvn clean install"
       };
-      # Runs when the workspace is (re)started
+      # Runs when a workspace is (re)started
       onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        run-server = "gradle bootRun"; # CHANGED: Commande Gradle pour lancer l'application Spring Boot
       };
     };
   };
